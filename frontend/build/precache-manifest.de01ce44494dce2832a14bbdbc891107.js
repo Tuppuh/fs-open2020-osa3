b6aacfa33f7d1e169673003ea867859683a0bbba
Tuomo Ojaluoma <ojaluoma.tuomo@gmail.com>
2020-09-26 14:55:05 +0300

    frontend production build

---
+++ frontend/build/precache-manifest.de01ce44494dce2832a14bbdbc891107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1cb3329558744650f048f4f4ebbf8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef768239585c89c54a35",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "740e955edf9b73a83760",
+    "url": "/static/js/2.584d881a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.584d881a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef768239585c89c54a35",
+    "url": "/static/js/main.8956069b.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);

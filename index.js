0061504faec838289c6f64b7751596f3cb109e30
Tuomo Ojaluoma <ojaluoma.tuomo@gmail.com>
2020-09-27 20:54:25 +0300

    puhelin + mongo steps 3,4

--- index.js
+++ index.js
@@ -47,8 +47,7 @@ app.get('/api/persons', (req, res) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
         if (person){
             response.json(person.toJSON())
@@ -56,6 +55,10 @@ app.get('/api/persons/:id', (request, response) => {
         else{
             response.status(404).end()
         }
+    })
+    .catch(error => {
+        console.log(error)
+        next(error)
     })
 })
 
@@ -96,12 +99,31 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
